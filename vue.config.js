201f94d0b50f7954752240b6aedf14db3329661f
Owen <guangchao.yu@bluefocus.com>
2024-09-10 08:59:37 +0800

    修改子路由刷新404问题

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
+    assetsPublicPath: '/',
     publicPath: '/chase-2design/',
     devServer:{
         host: '0.0.0.0',
